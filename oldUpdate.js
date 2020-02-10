8c7e01ee459e15fc26f1652b8e1fbc9bf78a86f0
M469 <60880131+M469@users.noreply.github.com>
2020-02-10 15:57:10 +0300

    Update oldUpdate.js

--- oldUpdate.js
+++ oldUpdate.js
@@ -1,1 +1,28 @@
-console.log("hello")
+const request = require("request");
+const fs = require("fs");
+const readline = require("readline-sync");
+const input = readline.question;
+const download = require("download-git-repo");
+var version = "1.0";
+
+function checkUpdate() {
+request('https://raw.githubusercontent.com/M469/Testing-Something/master/version', function(error, response, body) {
+    if(body > version) {
+        var YesNo = readline.keyInYN("there is new update release do you want to download it ?");
+        if(YesNo === false) return;
+        if(YesNo === true) {
+            download('M469/Testing-Something', __dirname, function (err) {
+                console.log(err ? 'Error While Updating The Tool' : "Successfully Updated !, Please Start The Tool Again.")
+                process.exit(1);
+            })
+        }
+    } else {
+        throw error;
+    }
+})
+}
+
+checkUpdate()
+
+
+console.log("There is no new updates !");
